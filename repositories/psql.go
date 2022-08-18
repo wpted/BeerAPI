17bb92f32998bc68822d97fedbc1b18675a73649
wpted <silverhandsome@gmail.com>
2022-08-18 15:11:43 +0800

    Create repo logic for PSQL

---
+++ repositories/psql.go
@@ -0,0 +1,75 @@
+package repositories
+
+import (
+	"BeerAPI/model"
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+type PostgreSQL struct {
+	DB *sql.DB
+}
+
+// -------------------- Connection Logic ---------------------
+
+func NewPostgreSQL() (*PostgreSQL, error) {
+	db, err := sql.Open("postgres", "postgres://beer_fellow:lovebeer@localhost/beer_server?sslmode=disable")
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	fmt.Println("You've successfully connected to PostgreSQL")
+	return &PostgreSQL{DB: db}, nil
+
+}
+
+func (p *PostgreSQL) Close() {
+	p.DB.Close()
+}
+
+// -------------------- Access Database ---------------------
+
+func (p *PostgreSQL) CreateBeer(b model.Beer) {
+	insertQuery := " INSERT INTO beers (name, price, company) VALUES($1, $2, $3)"
+	_, err := p.DB.Exec(insertQuery, b.Name, b.Price, b.Company)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	fmt.Println(b)
+}
+
+func (p *PostgreSQL) GetBeers() []model.Beer {
+	beers := make([]model.Beer, 0)
+	getBeersQuery := "SELECT * FROM beers"
+	rows, err := p.DB.Query(getBeersQuery)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	defer rows.Close()
+	for rows.Next() {
+		beer := model.Beer{}
+		rows.Scan(&beer.ID, &beer.Name, &beer.Price, &beer.Company)
+		beers = append(beers, beer)
+	}
+	return beers
+}
+
+func (p *PostgreSQL) GetBeer(id int) model.Beer {
+	beer := model.Beer{}
+	getBeerQuery := "SELECT * FROM beers WHERE ID=$1"
+	row := p.DB.QueryRow(getBeerQuery, id)
+	err := row.Scan(&beer.ID, &beer.Name, &beer.Price, &beer.Company)
+	switch {
+	case err == sql.ErrNoRows:
+		return model.Beer{}
+	case err != nil:
+		return model.Beer{}
+	default:
+		return beer
+	}
+}
