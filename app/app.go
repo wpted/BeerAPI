68a8e56a7393520a422233144fb00448191dfa4e
wpted <silverhandsome@gmail.com>
2022-08-18 16:27:57 +0800

    Add enpoints and defaultServeMux using net/http

--- app/app.go
+++ app/app.go
@@ -1,15 +1,18 @@
 package app
 
 import (
+	"BeerAPI/controller"
 	"BeerAPI/repositories"
 	"context"
 	"log"
+	"net/http"
 )
 
 var (
 	Ctx       context.Context
 	redisRepo *repositories.Redis
 	psqlRepo  *repositories.PostgreSQL
+	handler   *controller.BeerHandler
 )
 
 func init() {
@@ -22,9 +25,13 @@ func init() {
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-
+	handler = &controller.BeerHandler{PostgreSQL: psqlRepo}
 }
 
 func Run() {
-
+	defer psqlRepo.Close()
+	defer redisRepo.Close()
+	http.HandleFunc("/beer", handler.GetBeerByID)
+	http.HandleFunc("/beers", handler.GetAllBeers)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
