4abc56243668b276ef484d6e89880dec9078780c
wpted <silverhandsome@gmail.com>
2022-08-22 16:40:06 +0800

    Add comments.

--- util/utilities.go
+++ util/utilities.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 )
 
+// RenderJson encodes and writes the given jsonified value to the w
+// , simultaneously setting the status code to the header
 func RenderJson(w http.ResponseWriter, val any, statusCode int) {
 	w.WriteHeader(statusCode)
 	_ = json.NewEncoder(w).Encode(val)
