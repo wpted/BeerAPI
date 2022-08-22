05f2116fd6e10c840388f47d70fc80dc4e5adef3
wpted <silverhandsome@gmail.com>
2022-08-22 16:42:22 +0800

    Made sure the cahing and fetching from the database works within GetBeerByID. Add comments.

--- controller/controller.go
+++ controller/controller.go
@@ -1,57 +1,69 @@
 package controller
 
 import (
+	"BeerAPI/model"
 	"BeerAPI/repositories"
 	"BeerAPI/util"
-	"fmt"
+	"context"
+	"encoding/json"
+	"log"
 	"net/http"
 	"strconv"
 )
 
+// BeerHandler is a struct that contains a pool of the PSQL connection and the Redis connection
 type BeerHandler struct {
 	*repositories.PostgreSQL
 	*repositories.Redis
 }
 
-func (b *BeerHandler) CreateBeer(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("content-type", "application/json")
-	//id := r.FormValue("id")
+//func (b *BeerHandler) CreateBeer(w http.ResponseWriter, r *http.Request) {
+//	w.Header().Set("content-type", "application/json")
+//	//id := r.FormValue("id")
+//
+//	//b.InsertBeer(id)
+//
+//}
 
-	//b.InsertBeer(id)
-
-}
-
+// GetBeerByID is a handler that gets beer info from the database with the given id and writes is as json to the http response
 func (b *BeerHandler) GetBeerByID(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
 	idString := r.FormValue("id")
-
 	// check if data exist in Redis
-	fmt.Println(idString)
-	val, err := b.Redis.GetFromRedis(r.Context(), idString)
-	fmt.Println(val)
+	val, err := b.Redis.GetFromRedis(context.Background(), idString)
 	if err == nil {
+		cachedBeer := model.Beer{}
+		// decode the redis string value back into a struct
+		err = json.Unmarshal([]byte(val), &cachedBeer)
+		if err != nil {
+			// if anything wrong it must be the retrieved value doesn't bind with the beer struct
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 		// if data exist return the cached value to the response
-		util.RenderJson(w, val, http.StatusOK)
+		util.RenderJson(w, cachedBeer, http.StatusOK)
 		return
 	}
-	// if doesn't exist convert the id string to id int
+	// if the data doesn't exist convert the id string to id int
 	id, err := strconv.Atoi(idString)
 	if err != nil {
 		http.Error(w, http.StatusText(400)+", please check your input", http.StatusBadRequest)
 	}
 	// get value corresponding to the id from the PostgreSQL server
 	result := b.GetBeer(id)
-
+	resultJson, err := result.MarshalBinary()
 	// after getting from psql, add to the cache pool
-	b.Redis.AddToRedis(r.Context(), idString, result, 0)
-	// send the data back to the response in json form
+	err = b.Redis.AddToRedis(context.Background(), idString, resultJson, 0)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	//// send the data back to the response in json form
 	util.RenderJson(w, result, http.StatusOK)
 }
 
+// GetAllBeers is a handler that gets all existing beer info within the database and writes it as a json to the http response
 func (b *BeerHandler) GetAllBeers(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
-	requestedBeers := b.GetBeers()
-
-	fmt.Fprint(w, requestedBeers)
-
+	result := b.GetBeers()
+	util.RenderJson(w, result, http.StatusOK)
 }
