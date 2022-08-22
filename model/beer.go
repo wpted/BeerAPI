8f35cbf8606f26f159eb056c6785e8b5eb0efa5e
wpted <silverhandsome@gmail.com>
2022-08-22 16:40:54 +0800

    Add function MarshalBinary() for encoding usage.

--- model/beer.go
+++ model/beer.go
@@ -1,8 +1,23 @@
 package model
 
+import (
+	"encoding/json"
+	"log"
+)
+
+// Beer is a model that matches the field within the PSQL database
 type Beer struct {
 	ID      int    `json:"id"`
 	Name    string `json:"name"`
 	Price   int    `json:"price"`
 	Company string `json:"company"`
+}
+
+// MarshalBinary marshals the beer struct and return a []byte and an error
+func (b Beer) MarshalBinary() ([]byte, error) {
+	json, err := json.Marshal(b)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	return json, err
 }
