ec0590f72bc2c29c2217ec2da44afa6839b69687
wpted <silverhandsome@gmail.com>
2022-08-22 17:07:34 +0800

    Update database error handling string.

--- app/app.go
+++ app/app.go
@@ -19,11 +19,14 @@ func init() {
 	var err error
 
 	psqlRepo, err = repositories.NewPostgreSQL()
+	if err != nil {
+		log.Fatalf("Couldn't intialize PostgreSQL connection: %s", err.Error())
+	}
 	// initialize Ctx as an empty context for demo, shouldn't be empty
 	Ctx = context.Background()
 	redisRepo, err = repositories.NewRedisClient(Ctx)
 	if err != nil {
-		log.Fatal(err.Error())
+		log.Fatalf("Couldn't intialize Redis connection: %s", err.Error())
 	}
 	// connect initialized repos with the handler
 	handler = &controller.BeerHandler{PostgreSQL: psqlRepo, Redis: redisRepo}
