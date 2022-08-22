1b2a4f2cfa245d33cfba4c81b4294ba3496f0610
wpted <silverhandsome@gmail.com>
2022-08-22 16:43:21 +0800

    Add comments.

--- repositories/redis.go
+++ repositories/redis.go
@@ -13,30 +13,34 @@ type Redis struct {
 
 // -------------------- Connection Logic ---------------------
 
+// NewRedisClient takes input context and return a pointer to Redis struct.
+// If connection failed, return nil and the given error
 func NewRedisClient(ctx context.Context) (*Redis, error) {
 	connectOptions := redis.Options{
 		Addr:     "localhost:6379",
 		Password: "",
 		DB:       0,
 	}
-
 	client := redis.NewClient(&connectOptions)
 
 	_, err := client.Ping(ctx).Result()
 	if err != nil {
 		return nil, err
 	}
-
+	fmt.Println("You've successfully connected to Redis.")
 	return &Redis{client}, nil
 
 }
 
+// Close shuts the redis connection down
 func (r *Redis) Close() {
 	r.Client.Close()
 }
 
 // -------------------- Access Database ---------------------
 
+// AddToRedis sets the given Key-Value pair within Redis, and return an empty error if succeeded.
+// If process failed, return the given error.
 func (r *Redis) AddToRedis(ctx context.Context, key string, value any, expiresAt time.Duration) error {
 	err := r.Client.Set(ctx, key, value, expiresAt).Err()
 	if err != nil {
@@ -45,11 +49,13 @@ func (r *Redis) AddToRedis(ctx context.Context, key string, value any, expiresAt
 	return nil
 }
 
+// GetFromRedis gets the Value from the given Key within Redis, returns the values string and an empty error if succeeded.
+// If process failed, return an empty string and the given error.
 func (r *Redis) GetFromRedis(ctx context.Context, key string) (string, error) {
 	result, err := r.Client.Get(ctx, key).Result()
 	switch {
 	case err == redis.Nil:
-		fmt.Printf("%s does not exist within the cache pool", key)
+		fmt.Printf("%s does not exist within the cache pool\n", key)
 		return "", err
 	case err != nil:
 		fmt.Println("Something went wrong with the redis server")
